cb7c83ca7b9e5bc9fb11aea008aa7f23634a7674
ivan dolgolaptev <ivandolgolaptev@gmail.com>
2024-05-02 16:09:37 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Иван Долголаптев on 2.05.24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
